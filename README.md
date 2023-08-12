7c7f7698fdb73d0ba9b6a2ea5f6b1696f4c51e6b
Neidz <adamo7255@gmail.com>
2023-08-12 21:34:32 +0200

    feature: adds more tests for CommandLineHandler

---
+++ README.md
@@ -0,0 +1,6 @@
+# Running project
+From PixelArtAnalyzer/src run:
+
+```
+dotnet run -sourceImageLocation "../Resources/SampleImages/final_2023_place.png" -targetImageLocation "../Resources/SampleImages/crewmate.png"
+```
---
+++ tests/CommandLineHandler/CommandLineHandlerTests.cs
@@ -0,0 +1,71 @@
+namespace Tests.CommandLineHandler;
+
+using PixelArtAnalyzer.CommandLineHandler;
+using SixLabors.ImageSharp;
+using Xunit;
+
+
+
+public class CommandLineHandlerUtilsTests
+{
+    public class TryParseRgbaTests
+    {
+        [Fact]
+        public void CorrectlyParsesRgba()
+        {
+            string validValue = "rgba(100,150,200,255)";
+
+            Rgba32 result = CommandLineHandlerUtils.TryParseRgba(validValue);
+
+            Assert.Equal(100, result.R);
+            Assert.Equal(150, result.G);
+            Assert.Equal(200, result.B);
+            Assert.Equal(255, result.A);
+        }
+
+        [Fact]
+        public void CorrectlyParsesRgb()
+        {
+            string validValue = "rgb(200,100,200)";
+
+            Rgba32 result = CommandLineHandlerUtils.TryParseRgba(validValue);
+
+            Assert.Equal(200, result.R);
+            Assert.Equal(100, result.G);
+            Assert.Equal(200, result.B);
+            Assert.Equal(255, result.A);
+        }
+
+        [Fact]
+        public void FailsForHex()
+        {
+            string invalidValue = "#000000";
+
+            Assert.Throws<ArgumentException>(() => CommandLineHandlerUtils.TryParseRgba(invalidValue));
+        }
+    }
+
+    public class TryParseHexTests
+    {
+        [Fact]
+        public void CorrectlyParsesHex()
+        {
+            string validValue = "#6496c8";
+
+            Rgba32 result = CommandLineHandlerUtils.TryParseHex(validValue);
+
+            Assert.Equal(100, result.R);
+            Assert.Equal(150, result.G);
+            Assert.Equal(200, result.B);
+            Assert.Equal(255, result.A);
+        }
+
+        [Fact]
+        public void FailsForRgb()
+        {
+            string invalidValue = "rgb(200,100,200)";
+
+            Assert.Throws<ArgumentException>(() => CommandLineHandlerUtils.TryParseHex(invalidValue));
+        }
+    }
+}
